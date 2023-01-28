52a26294f842bb0e1a23870334809093b4e0c29a
Redcowbois <jiayang@outlook.fr>
2023-01-28 17:05:37 -0500

    half functioning data base

--- reader/models.py
+++ reader/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model): #Class for the user
+    name = models.CharField(max_length=200)
+
+    def __str__(self): 
+        return str(self.name)
+
+class Course(models.Model): #Class for each different course 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.CharField(max_length=200)
+    start_time = models.CharField(max_length=100)
+    end_time = models.CharField(max_length=100)
+
+    def __str__(self): 
+        return str(self.course)
