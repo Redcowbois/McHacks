fd0e7af7cb548d9d30803f06b873f0f0ad034488
Fabian Saldana <114820909+FabianSaldana@users.noreply.github.com>
2023-01-29 08:38:05 -0500

    bjjb

--- reader/views.py
+++ reader/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
-<<<<<<< HEAD
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+#<<<<<<< HEAD
+#from django.contrib.auth import login, authenticate
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 from django.http import HttpResponse 
 
 
 # Create your views here.
 
 def register(response):
-    form = UserCreationForm()
-    return render(response, "register/register.html", {"forms":form})
+    if response.method == "POST":
+        #form = UserCreationForm(response.POST)
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        
+        return redirect("/home")
+    else:
+        form = RegisterForm()
+
+    return render(response, "register.html", {"form":form})
 
 def view(request):
     return render(request, 'login.html')
